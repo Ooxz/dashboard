9c06ffd53ea6549bb6d5c6294d34ccb377a7c634
Brune <abrune.osteo@gmail.com>
2022-11-22 22:28:54 +0100

    fetch background image

---
+++ index.js
@@ -0,0 +1,6 @@
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+.then(response => response.json())
+.then(data => {
+	document.body.style.backgroundImage = `url(${data.urls.regular})`
+	document.getElementById("author").innerHTML = `By: ${data.user.name}`
+})
